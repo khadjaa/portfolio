568e8a1414fcc0332ebcb074cfa6eb277ad3dacc
khadjaa <islomkhadja2001@icloud.com>
2023-05-28 15:27:28 +0300

    added styles

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import styles from './Skills.module.css'
 import styleContainer from '../common/styles/Container.module.css'
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
+
+    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores autem debitis dignissimos distinctio ex harum illum, ipsum libero ratione rem saepe'
+
     return (
         <div className={styles.skillsBlock}>
             <div className={`${styleContainer.container} ${styles.skillsContainer}`}>
                 <h2 className={styles.styleTitle}>Skills</h2>
                 <div className={styles.skills}>
-                    hkhk
+                    <Skill title={'JS'} description={lorem}/>
+                    <Skill title={'React'} description={lorem}/>
+                    <Skill title={'CSS'} description={lorem}/>
                 </div>
             </div>
         </div>
--- src/components/skills/skill/Skill.tsx
+++ src/components/skills/skill/Skill.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import styles from './Skills.module.css'
+import styles from './Skill.module.css'
 
-export const Skill = () => {
+type SkillPropsType = {
+    title: string
+    description: string
+}
+
+export const Skill = (props: SkillPropsType) => {
     return (
         <div className={styles.skill}>
-
+            <div className={styles.icon}></div>
+            <h3>{props.title}</h3>
+            <span className={styles.description}>
+                {props.description}
+            </span>
         </div>
     );
 };
